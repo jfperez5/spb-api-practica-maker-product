952d4b1c7dd2b05ba10c9b5315c695eac6d71b77
jfperez5 <65435098+jfperez5@users.noreply.github.com>
2025-08-14 21:55:46 -0600

    Se agrega el metodo save and update de Makers

--- src/main/java/com/caneksoft/peliculas/controller/MakerController.java
+++ src/main/java/com/caneksoft/peliculas/controller/MakerController.java
@@ -1,5 +1,7 @@
 package com.caneksoft.peliculas.controller;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Optional;
 
@@ -7,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -49,5 +54,43 @@ public class MakerController {
                 .build())
             .toList();
     }
+// Otra forma de listar los fabricantes
+    @GetMapping("/findAll")
+    public ResponseEntity<?> buscarTodos(){
+        List<MakerDTO> makerList = makerService.findAll()
+        .stream()
+        .map(maker ->MakerDTO.builder()
+        .id(maker.getId())
+        .name(maker.getName())
+        .build())
+        .toList();
+        return ResponseEntity.ok(makerList);
+    }
 
+    @PostMapping("/save")
+    public ResponseEntity<?>save(@RequestBody MakerDTO makerDTO) throws URISyntaxException{
+        if(makerDTO.getName().isBlank()){
+            return ResponseEntity.badRequest().build();
+        }
+        makerService.save(Maker.builder()
+            .name(makerDTO.getName())
+            .build()
+        );
+        return ResponseEntity.created(new URI("/api/maker/save")).build();
+    }
+    
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?>updateMaker(@PathVariable Long id, @RequestBody MakerDTO makerDTO){
+        Optional<Maker>makerOptional = makerService.findById(id);
+
+        if(makerOptional.isPresent()){
+            Maker maker =makerOptional.get();
+            maker.setName(makerDTO.getName());
+            makerService.save(maker);
+            return ResponseEntity.ok("Registro actualizado");
+        }
+
+        return ResponseEntity.notFound().build();
+    }
 }
